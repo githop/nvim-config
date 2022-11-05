9d31cdafa22ba720229dc0695414fae3e02528e7
githop <tom@githop.com>
2022-11-04 18:43:19 -0600

    add gitsigns plugin

--- lua/githop/plugins-setup.lua
+++ lua/githop/plugins-setup.lua
@@ -74,6 +74,9 @@ return packer.startup(function(use)
 	use("windwp/nvim-autopairs")
 	use("windwp/nvim-ts-autotag")
 
+	-- git
+	use("lewis6991/gitsigns.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
